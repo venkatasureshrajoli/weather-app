133f760bba073b485586f4a9db04f3d5232edd32
Venkata Suresh Rajoli <venkatasuresh.rajoli@gmail.com>
2021-10-30 18:40:50 +0530

    API url to use https

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 function Weather() {
 
     useEffect(() => {
-        const URL = `http://api.openweathermap.org/data/2.5/weather?q=solapur&units=metric&appid=687dd8f716b13ad1c5c37ff9fd5ccd73`;
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=solapur&units=metric&appid=687dd8f716b13ad1c5c37ff9fd5ccd73`;
         fetch(URL).then((response) => {
             console.log('the response is', response);
             const json = response.json();
