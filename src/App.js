8f133788e6426aec6b0b191d5285ac533ba5a96d
Venkata Suresh Rajoli <venkatasuresh.rajoli@gmail.com>
2021-11-06 09:45:08 +0530

    Changed the content to Hello World

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Header Content
+        Hello World
       </header>
       <Weather/>
     </div>
